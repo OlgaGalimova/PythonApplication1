4e8c6ae4e13e4abdc48254109eacb930378b1915
Nexus <Nexus@DESKTOP-FH8H1FP>
2024-01-10 14:05:37 +0100

    Vigade otsing  (tehtud)

--- PythonApplication1/vigade otsingpy.py
+++ PythonApplication1/vigade otsingpy.py
@@ -2,15 +2,15 @@
 #Kopeeri antud kood failisse. Otsi ja paranda programmis vigu
 #Lisa kommentaari, kus kirjuta mis oli parandatud.
 
-import math#import * from math
+import math#import * from import math * - не надо писать в  дальнейшем 
 
 
 print("Ruudu karakteristikud")#print("Ruudu karakteristikud")
 a= float(input('Sisesta ruudu külje pikkus => '))#a=input('Sisesta ruudu külje pikkus => ')
 S=a**2#S=a**2
-print("Ruudu pindala", round(S,1))
+print("Ruudu pindala", round(S,1))#print("Ruudu pindala", S)
 P=4*a#P=4*a
-print("Ruudu ümbermõõt: ", round(P,1)) 
+print("Ruudu ümbermõõt: ", round(P,1)) #print("Ruudu ümbermõõt'', P)
 di=a*math.sqrt(2)#di=a*math.sqr(2)
 print("Ruudu diagonaal: ", round(di,2))#print("Ruudu diagonaal", round(di,2))
 print()#print()
@@ -20,17 +20,17 @@ b=float(input("Sisesta ristküliku 1. külje pikkus => "))#b=input("Sisesta rist
 c=float(input("Sisesta ristküliku 2. külje pikkus => "))#c=input("Sisesta ristküliku 2. külje pikkus => ")
 S=b*c#S=b*c
 print("Ristküliku pindala:", S)#print(Ristküliku pindala', S)
-P=2*(b+c)
-print("Ristküliku ümbermõõt:", P)
-di=math.sqrt(b**2+c**2)
-print("Ristküliku diagonaal:",round(di,1))
-print()#otstup
+P=2*(b+c)#P=2(b+c)
+print("Ristküliku ümbermõõt:", P)#print("Ristküliku ümbermõõt", P)
+di=math.sqrt(b**2+c**2)#di=math.sqrt(b*2+c*2)
+print("Ristküliku diagonaal:",round(di,1))#print("Ristküliku diagonaal", round(di)
+print()#отступ
 
-print("Ringi karakteristikud")
-r=float(input("Sisesta ringi raadiusi pikkus => "))
-d=2*r
-print("Ringi läbimõõt:",d)
-S=math.pi * r**2
-print("Ringi pindala:", round(S,2))#2- okruglenie do dvuh znakov 
-C = 2 * math.pi * r
-print("Ringjoone pikkus:",round(C,2))
+print("Ringi karakteristikud")#print("Ringi karakteristikud")
+r=float(input("Sisesta ringi raadiusi pikkus => "))#r=input(''Sisesta ringi raadiusi pikkus => ''))
+d=2*r#d=2r
+print("Ringi läbimõõt:",d)#print("Ringi läbimõõt" d)
+S=math.pi * r**2#S=pi()*r*2
+print("Ringi pindala:", round(S,2))#2- округление до двух знаков
+C = 2 * math.pi * r#C=2pi()*r
+print("Ringjoone pikkus:",round(C,2))#print("Ringjoone pikkus", round(C)
