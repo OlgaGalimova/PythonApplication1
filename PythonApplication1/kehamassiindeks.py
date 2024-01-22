4d500c1fd1825a27132db1d2dd0fdebddf1109b3
Olga Galimova <olga.galimova@tthkee.onmicrosoft.com>
2024-01-22 03:44:16 +0200

    Viimane!

--- PythonApplication1/kehamassiindeks.py
+++ PythonApplication1/kehamassiindeks.py
@@ -1,47 +1,68 @@
+## if, else -
+#print("Tere! Olen sinu uus sõber - Python!")
+#nimi = input("Sisesta oma nimi: ")
+#print(nimi + ", oi kui ilus nimi!")
+#vastus = int(input(nimi + "! Kas leian Sinu keha indeksi? 0-ei, 1-jah => "))
+#if vastus =="1":
+#    print("Indexi leidmine")
+#else:
+#    print("Kahju! See on väga kasulik info!")
+#    print()
+#    print("Kohtumiseni, " + nimi + "! Igavesti Sinu, Python!")
 
+# Находим дополнительные значения вместе с проверкой try, except
+##try, except - ошибоки, которые могут возникнуть во время выполнения программы
+#print("Tere! Olen sinu uus sõber - Python!")
+#nimi = input("Sisesta oma nimi: ")
+#print(nimi + ", oi kui ilus nimi!")
+#vastus = int(input(nimi + "! Kas leian Sinu keha indeksi? 0-ei, 1-jah => "))
+#if vastus =="1":
+#    print("Indexi leidmine")
+#    try:
+#        pikkus=int(input("Pikkus: "))
+#        mass=float(input("Kaal: "))
+#    except :
+#        print("Viga!")
+#        print("Kahju! See on väga kasulik info!")
+#        print()
+#        print("Kohtumiseni, " + nimi + "! Igavesti Sinu, Python!")
+
+#while True - повторение цикла try; except     
 print("Tere! Olen sinu uus sõber - Python!")
 nimi = input("Sisesta oma nimi: ")
 print(nimi + ", oi kui ilus nimi!")
 vastus = int(input(nimi + "! Kas leian Sinu keha indeksi? 0-ei, 1-jah => "))
-#if vastus == "1":#
-
-#    try:
-#        # присваивание переменной pikkus целого значения переменной, введенной пользователем
-#        pikkus = int(input("Sisesta oma pikkus: "))
-#        # присваивание переменной mass значения выражения в формате действительного числа
-#        mass = float(input("Sisesta oma kaal: "))
-#        #присваивание переменной indeks значения выражения mass /(0.01pikkus)2
-#        indeks = mass / (0.01 * pikkus)**2
-#        # вывод на экран имени, объединённого с текстом "! Sinu keha indeks on:" indeks с одним знаком после запятой
-#        print(nimi + "! Sinu keha indeks on: ", round(indeks,1))
-#        #вывод оценки индекса массы тела в соответствии с таблицей ниже:
-#        print("Indeksi hinnang:")
-#        if indeks < 16:
-#            print("Tervisele ohtlik alakaal")
-#        elif 16 <= indeks < 19:
-#            print("Alakaal")
-#        elif 19 <= indeks < 25:
-#            print("Normaalkaal")
-#        elif 25 <= indeks < 30:
-#            print("Ülekaal")
-#        elif 30 <= indeks < 35:
-#            print("Rasvumine")
-#        elif 35 <= indeks < 40:
-#            print("Tugev rasvumine")
-#        else:
-#            print("Tervisele ohtlik rasvumine")
-
 if vastus ==1:
     print("Indexi leidmine")
-    while True:
+    while True:#повторение цикла
         try:
             pikkus=int(input("Pikkus: "))
             mass=float(input("Kaal: "))
             break
         except:
             print("Viga!")
-    index=mass/(0.01*pikkus)**2
+    index = mass/(0.01*pikkus)**2  #присваивание переменной indeks значения выражения mass /(0.01pikkus)2
+    #вывод на экран имени, объединённого с текстом "! Sinu keha indeks on:" indeks с одним знаком после запятой
+    print(nimi + "! Sinu keha indeksi on: ", round(index,1))
+    #вывод оценки индекса массы тела в соответствии с таблицей ниже:
+    print("Indeksi hinnang:")
     if index<16:
-        print
+      print("Tervisele ohtlik alakaal")  
+    elif 16 <= index < 19:  
+      print("Alakaal")    
+    elif 19 <= index < 25:    
+      print("Normaalkaal")      
+    elif 25 <= index < 30:    
+      print("Ülekaal")      
+    elif 30 <= index < 35:    
+      print("Rasvumine")     
+    elif 35 <= index < 40:    
+      print("Tugev rasvumine")      
+    else:    
+     print("Tervisele ohtlik rasvumine")      
+        
+            
+
+
 
    
--- PythonApplication1/kordused.py
+++ PythonApplication1/kordused.py
@@ -1,58 +1,58 @@
-#1 Вводят 15 чисел. Определить, сколько среди них целых.
-print("1. variant")
-k=0
-mitu=0
-while k<15:
-    k+=1
-    n=float(input("Sisesta arv nr." +str (k)))
-    if int(n)==float(n):
-        mitu+=1
+##1 Вводят 15 чисел. Определить, сколько среди них целых.
+#print("1. variant")
+#k=0
+#mitu=0
+#while k<15:
+#    k+=1
+#    n=float(input("Sisesta arv nr." +str (k)))
+#    if int(n)==float(n):
+#        mitu+=1
   
-print("2. variant")
-k=0
-mitu=0
-while True:
-    k+=1
-    n=float(input("Sisesta arv nr." +str (k)))
-    if int(n)==float(n):
-        mitu+=1
-    if k==15: break 
+#print("2. variant")
+#k=0
+#mitu=0
+#while True:
+#    k+=1
+#    n=float(input("Sisesta arv nr." +str (k)))
+#    if int(n)==float(n):
+#        mitu+=1
+#    if k==15: break 
     
-print("3. variant")
+#print("3. variant")
 
-mitu=0
-for k in range (1,16):
-    n=float(input("Sisesta ", +str(k), "arv"))
-    if int(n)==float(n):
-        mitu+=1
-print("Taisarvude kogus: ",mitu)
+#mitu=0
+#for k in range (1,16):
+#    n=float(input("Sisesta ", +str(k), "arv"))
+#    if int(n)==float(n):
+#        mitu+=1
+#print("Taisarvude kogus: ",mitu)
 
-#2. Запросите у пользователя число А и найдите сумму всех натуральных чисел от 1 до А
+##2. Запросите у пользователя число А и найдите сумму всех натуральных чисел от 1 до А
 
-print("1. variant")
+#print("1. variant")
 
-mitu=0
-A=int(input("Введите число A: "))
-summa = 0
-# Цикл для суммирования натуральных чисел от 1 до A
-for k in range(1, A + 1):
-    n = float(input("Sisesta число. arv: "))
-    if int(n)==float(n):
-      mitu+=k
-# Вывод результата
-print("Сумма всех натуральных чисел от 1 до A равна mitu")
+#mitu=0
+#A=int(input("Введите число A: "))
+#summa = 0
+## Цикл для суммирования натуральных чисел от 1 до A
+#for k in range(1, A + 1):
+#    n = float(input("Sisesta число. arv: "))
+#    if int(n)==float(n):
+#      mitu+=k
+## Вывод результата
+#print("Сумма всех натуральных чисел от 1 до A равна mitu")
 
 
 
-#3.    Вводят 8 чисел. Найти их произведение (только положительных).
-print("1. variant")
-from random import *
-p=1
-for i in range(8):
-    a=randint(-10,10)
-    print(a)
-    if a>0:p*=a
-print(p)
+##3.    Вводят 8 чисел. Найти их произведение (только положительных).
+#print("1. variant")
+#from random import *
+#p=1
+#for i in range(8):
+#    a=randint(-10,10)
+#    print(a)
+#    if a>0:p*=a
+#print(p)
         
 
 
@@ -61,48 +61,81 @@ print(p)
 
 
 
-print()
+#print()
 
-for i in range(10,0,-2):
-    print(i)
+#for i in range(10,0,-2):
+#    print(i)
 
-for i in range(15): #1-14, samm =1
-    print(i,"samm")
+#for i in range(15): #1-14, samm =1
+#    print(i,"samm")
 
-for i in rahge(10):
-    n=input("Nimi: ")
-    print("\tTere",n)
+#for i in rahge(10):
+#    n=input("Nimi: ")
+#    print("\tTere",n)
 
-print()
-vastus=""
-while vastus.lower()!="komm":
-    vastus=input("Tahan kommi!")
+#print()
+#vastus=""
+#while vastus.lower()!="komm":
+#    vastus=input("Tahan kommi!")
 
-print()
+#print()
 
-while True:
-    vastus=input("Tahan kommi!")
-    if vastus.lower()=="komm":
-        break
-    else:
-        print("Koik raagivad "+vastus)
+#while True:
+#    vastus=input("Tahan kommi!")
+#    if vastus.lower()=="komm":
+#        break
+#    else:
+#        print("Koik raagivad "+vastus)
 
-print()
-while True: # lopmatu tsukkel
-    try:
-        pikkus=int(input("pikkus: "))
-        laius=int(input("laius: "))
-        if pikkus>0 and laius>0:
-            print("Pindala:", pikkus*laius)
-            print("Pikkus ja laius on sisestatud ja pindala on leidnud")
-        else:
-            print("on vaja andmeid suurem kui 0")
-            print("Pikkus ja laius on sisestatud ja pindala ei ole leidnud")
-        break
-    except:
-        print("on vaja int format kasutada!")# ilmub ekraanil 
+#print()
+#2
+#Ia
+#try:
+#    pikkus=int(input("pikkus: "))
+#except :
+#    print("on vaja int format kasutada!")# ilmub ekraanil 
 
-print("Töö lõpp")
+#Ib Можно продолжить вычисления. Спросить еще один параметр. Данный вариант, мы перезапускаем конструкцию
+#try:
+#    pikkus=int(input("pikkus: "))
+#    laius=int(input("laius: "))
+#except :
+#    print("on vaja int format kasutada!")# ilmub ekraanil 
 
+
+#IIa
+#while True:#lopetamatu tsukkel
+#    try:
+#        pikkus=int(input("pikkus: ")) #вместо PASS
+#        laius=int(input("laius: "))
+#    except :
+#        print("on vaja andmeid suurem kui 0") 
+
+##IIb
+#while True: #lõpmatu tsükkel
+#    try:
+#        pikkus=int(input("Pikkus: "))
+#        laius=int(input("Laius: "))
+#        print("Pindala:", pikkus*laius)
+#        break# прерывание 
+#    except :
+#        print("On vaja int formaat kasutada!") #ilmub ekraanil, kui viga andmetega
+    
+#III
+#while True: # lopetamatu tsukkel
+#    try:# проходить
+#        pikkus=int(input("pikkus: ")) # поставили вместо  pass. пытайся сделать следующее, узнавай pikkus
+#        laius=int(input("laius: "))
+#        if pikkus>0 and laius>0: #and – одновременно
+#            print("Pindala:", pikkus*laius)
+#            print("Pikkus ja laius on sisestatud ja pindala on leidnud")
+#        else:
+#            print("on vaja andmeid suurem kui 0")
+#            print("Pikkus ja laius on sisestatud ja pindala ei ole leidnud")
+#        break# прерывание 
+#    except:#кроме
+#        print("on vaja int format kasutada!")# ilmub ekraanil 
+
+#print("Töö lõpp")
 
 
